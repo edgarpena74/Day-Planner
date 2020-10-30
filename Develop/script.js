2cda2f767e75ebb6e17c53a13bc76c86087053f9
Edgar Pena <edgarignaciopena@gmail,com>
2020-10-29 21:13:09 -0700

    update

--- Develop/script.js
+++ Develop/script.js
@@ -2,14 +2,14 @@ let todo = document.getElementById("to-do");
 let save = document.getElementById("save");
 let input = document.getElementById("input");
 
-$(input).on("click", function(event){
+$("input").on("click", function(event){
    event.preventDefault();
    console.log("did it work?")
-   return;
+   
 });
 
 
-$(save).on("click", function(){
-    console.log("ive been clicked")
-    let text = document.createElement("p")
-})
+// $(save).on("click", function(){
+//     console.log("ive been clicked")
+    
+// })
