93f9f4dd5e9a6867bd60d9b9a52649a65262cc4d
Edgar Pena <edgarignaciopena@gmail,com>
2020-10-29 22:42:50 -0700

    trying to get to-do section to turn red after save

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,9 @@ let todo = document.getElementById("to-do");
 let save = document.getElementById("save");
 let form = document.getElementById("theForm");
 let userTask = document.getElementById("userTask");
+let formControl = document.getElementsByClassName("form-Control");
 
-$("form").on("submit", function(event){
+$("#form").on("submit", function(event){
    event.preventDefault();
    console.log("it works!")
    
@@ -11,9 +12,15 @@ $("form").on("submit", function(event){
 
 
 
-$("save").on("click", function() {
+$("#save").on("click", 
+
+    function() {
     alert("button was clicked")
     console.log("saved was clicked")
+    formControl.style.background = "red !important";
+    console.log("should be red?")
+    // todo.value = "HOLY SHIT DOES IT WORK?"
 
+    }
 
-})
+)
