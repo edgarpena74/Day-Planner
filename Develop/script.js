5a9bed73ad806c047dafe6da17d95f36638b242b
Edgar Pena <edgarignaciopena@gmail,com>
2020-10-31 18:22:12 -0700

    JS is working properly

--- Develop/script.js
+++ Develop/script.js
@@ -2,10 +2,22 @@
 $(document).ready(function () {
 let todo = document.getElementById("to-do");
 let tenAmToDo = document.getElementById("tenAmToDo");
-let elevenAmToDo = document.getElementById("elevenAmToDo")
+let elevenAmToDo = document.getElementById("elevenAmToDo");
+let twelvePmToDo = document.getElementById("twelvePmToDo");
+let onePmToDo = document.getElementById("onePmToDo");
+let twoPmToDo = document.getElementById("twoPmToDo");
+let threePmToDo = document.getElementById("threePmToDo");
+let fourPmToDo = document.getElementById("fourPmToDo");
+let fivePmToDo = document.getElementById("fivePmToDo");
 let nineAmBtn = localStorage.getItem("9-AM");
 let tenAmBtn =  localStorage.getItem("10-AM");
 let elevenAmBtn =  localStorage.getItem("11-AM");
+let twelvePmBtn =  localStorage.getItem("12-PM");
+let onePmBtn =  localStorage.getItem("1-PM");
+let twoPmBtn =  localStorage.getItem("2-PM");
+let threePmBtn =  localStorage.getItem("3-PM");
+let fourPmBtn =  localStorage.getItem("4-PM");
+let fivePmBtn =  localStorage.getItem("5-PM");
 console.log(nineAmBtn);
 console.log(tenAmBtn);
 
@@ -27,6 +39,42 @@ $("#10AmForm").on("submit", function(event){
  });
 
  $("#11AmForm").on("submit", function(event){
+    event.preventDefault();
+    console.log("it works!")
+    
+ });
+
+ $("#12PmForm").on("submit", function(event){
+    event.preventDefault();
+    console.log("it works!")
+    
+ });
+
+ $("#1PmForm").on("submit", function(event){
+    event.preventDefault();
+    console.log("it works!")
+    
+ });
+
+ $("#2PmForm").on("submit", function(event){
+    event.preventDefault();
+    console.log("it works!")
+    
+ });
+
+ $("#3PmForm").on("submit", function(event){
+    event.preventDefault();
+    console.log("it works!")
+    
+ });
+
+ $("#4PmForm").on("submit", function(event){
+    event.preventDefault();
+    console.log("it works!")
+    
+ });
+
+ $("#5PmForm").on("submit", function(event){
     event.preventDefault();
     console.log("it works!")
     
@@ -58,7 +106,6 @@ function nineAmTask () {
     console.log("nineAmBtn is a  " + typeof nineAmBtn)
     if (nineAmBtn === null) {
         todo.style.background = "lightgreen"
-       
     }
 
 }
@@ -87,7 +134,6 @@ function tenAmTask () {
     console.log("tenAmBtn is a  " + typeof tenAmBtn)
     if (tenAmBtn === null) {
         tenAmToDo.style.background = "lightgreen"
-       
     }
 
 }
@@ -106,8 +152,6 @@ $("#saveElevenAm").on("click",
     window.localStorage.setItem("11-AM", JSON.stringify(elevenAmBtn))
     console.log(typeof elevenAmBtn)
   
-
-
     }
 )
 
@@ -122,10 +166,180 @@ function elevenAmTask () {
     }
 }
 
+$("#saveTwelvePm").on("click", 
+
+    function() {
+
+    alert("button was clicked");
+    console.log("saved was clicked");
+    twelvePmToDo.style.background = "red";
+    console.log("should be red?");
+    let twelvePmBtn = twelvePmToDo.value
+    twelvePmToDo.textContent = twelvePmBtn
+    console.log(twelvePmBtn)
+    window.localStorage.setItem("12-PM", JSON.stringify(twelvePmBtn))
+    console.log(typeof twelvePmBtn)
+  
+    }
+);
+
+
+function twelvePmTask () {
+    twelvePmToDo.value = twelvePmBtn
+    twelvePmToDo.style.background = "red";
+    console.log("twelvePmBtn is a  " + typeof twelvePmBtn)
+    if (twelvePmBtn === null) {
+        twelvePmToDo.style.background = "lightgreen"
+       
+    }
+};
+
+$("#saveOnePm").on("click", 
+
+    function() {
+
+    alert("button was clicked");
+    console.log("saved was clicked");
+    onePmToDo.style.background = "red";
+    console.log("should be red?");
+    let onePmBtn = onePmToDo.value
+    onePmToDo.textContent = onePmBtn
+    console.log(onePmBtn)
+    window.localStorage.setItem("1-PM", JSON.stringify(onePmBtn))
+    console.log(typeof onePmBtn)
+  
+    }
+);
+
+
+function onePmTask () {
+    onePmToDo.value = onePmBtn
+    onePmToDo.style.background = "red";
+    console.log("onePmBtn is a  " + typeof onePmBtn)
+    if (onePmBtn === null) {
+        onePmToDo.style.background = "lightgreen"
+    }
+};
+
+$("#saveTwoPm").on("click", 
+
+    function() {
+
+    alert("button was clicked");
+    console.log("saved was clicked");
+    twoPmToDo.style.background = "red";
+    console.log("should be red?");
+    let twoPmBtn = twoPmToDo.value
+    twoPmToDo.textContent = twoPmBtn
+    console.log(twoPmBtn)
+    window.localStorage.setItem("2-PM", JSON.stringify(twoPmBtn))
+    console.log(typeof twoPmBtn)
+  
+    }
+);
+
+
+function twoPmTask () {
+    twoPmToDo.value = twoPmBtn
+    twoPmToDo.style.background = "red";
+    console.log("twoPmBtn is a  " + typeof twoPmBtn)
+    if (twoPmBtn === null) {
+        twoPmToDo.style.background = "lightgreen"
+    }
+};
+
+$("#saveThreePm").on("click", 
+
+    function() {
+
+    alert("button was clicked");
+    console.log("saved was clicked");
+    threePmToDo.style.background = "red";
+    console.log("should be red?");
+    let threePmBtn = threePmToDo.value
+    threePmToDo.textContent = threePmBtn
+    console.log(threePmBtn)
+    window.localStorage.setItem("3-PM", JSON.stringify(threePmBtn))
+    console.log(typeof threePmBtn)
+  
+    }
+);
+
+
+function threePmTask () {
+    threePmToDo.value = threePmBtn
+    threePmToDo.style.background = "red";
+    console.log("threePmBtn is a  " + typeof threePmBtn)
+    if (threePmBtn === null) {
+        threePmToDo.style.background = "lightgreen"
+    }
+};
+
+$("#saveFourPm").on("click", 
+
+    function() {
+
+    alert("button was clicked");
+    console.log("saved was clicked");
+    fourPmToDo.style.background = "red";
+    console.log("should be red?");
+    let fourPmBtn = fourPmToDo.value
+    fourPmToDo.textContent = fourPmBtn
+    console.log(fourPmBtn)
+    window.localStorage.setItem("4-PM", JSON.stringify(fourPmBtn))
+    console.log(typeof fourPmBtn)
+  
+    }
+);
+
+
+function fourPmTask () {
+    fourPmToDo.value = fourPmBtn
+    fourPmToDo.style.background = "red";
+    console.log("fourPmBtn is a  " + typeof fourPmBtn)
+    if (fourPmBtn === null) {
+        fourPmToDo.style.background = "lightgreen"
+    }
+};
+
+$("#saveFivePm").on("click", 
+
+    function() {
+
+    alert("button was clicked");
+    console.log("saved was clicked");
+    fivePmToDo.style.background = "red";
+    console.log("should be red?");
+    let fivePmBtn = fivePmToDo.value
+    fivePmToDo.textContent = fivePmBtn
+    console.log(fivePmBtn)
+    window.localStorage.setItem("5-PM", JSON.stringify(fivePmBtn))
+    console.log(typeof fivePmBtn)
+  
+    }
+);
+
+
+function fivePmTask () {
+    fivePmToDo.value = fivePmBtn
+    fivePmToDo.style.background = "red";
+    console.log("fivePmBtn is a  " + typeof fivePmBtn)
+    if (fivePmBtn === null) {
+        fivePmToDo.style.background = "lightgreen"
+    }
+};
+
+
 
 
 nineAmTask();
 tenAmTask();
-elevenAmTask()
+elevenAmTask();
+twelvePmTask();
+onePmTask();
+twoPmTask();
+threePmTask();
+fourPmTask();
+fivePmTask();
 
 })
